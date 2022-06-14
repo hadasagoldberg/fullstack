f2375cf18e702a168fe5853beb3f99e4f0c9aca8
Hadasa <hadasagoldberg@gmail.com>
2022-06-13 23:28:01 -0300

    segundo commit

--- models/contacts.py
+++ models/contacts.py
@@ -1,8 +1,15 @@
 from utils.db import db
 
+
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30))
     apellido = db.Column(db.String(30))
     mail = db.Column(db.String(60))
     telefono = db.Column(db.String(15))
+
+    def __init__(self, nombre, apellido, mail, telefono):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.mail = mail
+        self.telefono = telefono
--- routes/contacts.py
+++ routes/contacts.py
@@ -1,23 +1,49 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from models.contacts import Contact
+from utils.db import db
 
 contacts = Blueprint('contacts', __name__)
 
 @contacts.route('/')
 def home():
-    return render_template('index.html')
+    contacts = Contact.query.all()
+    return render_template('index.html', contacts = contacts)
 
-@contacts.route('/new')
+@contacts.route('/new', methods=['POST'])
 def new():
-    return render_template('index.html')
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    mail = request.form['mail']
+    telefono = request.form['telefono']
 
-@contacts.route('/getall')
-def getall():
-    return render_template('index.html')
+    new_contact = Contact(nombre, apellido, mail, telefono)
+    db.session.add(new_contact)
+    db.session.commit()
 
-@contacts.route('/update')
-def update():
-    return render_template('index.html')
+   
+    return  redirect('/')
+    
 
-@contacts.route('/delete')
-def delete():
-    return render_template('index.html')
+@contacts.route('/update/<id>', methods = ['GET', 'POST'])
+def update(id):
+    contact = Contact.query.get(id)
+    if request.method == 'POST':
+        contact.nombre = request.form["nombre"]
+        contact.apellido = request.form["apellido"]
+        contact.mail = request.form["mail"]
+        contact.telefono = request.form["telefono"]
+
+        db.session.commit()
+
+        return redirect('/')
+    
+    return render_template('update.html', contact=contact)
+
+@contacts.route('/delete/<id>')
+def delete(id):
+    
+    contact = Contact.query.get(id)
+    db.session.delete(contact)
+    db.session.commit()
+    
+    return redirect('/')
